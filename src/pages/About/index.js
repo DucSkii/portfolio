d087dc5c87f712f8608ebc47cb96963806eb9b7d
DucSkii <ducvietdao@live.co.uk>
2020-12-21 18:31:58 +0000

    updated animation

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+
 import { useStyles } from './styles'
 
 const About = () => {
 
   const classes = useStyles()
+  const loading = useSelector(state => state.pageTransition.loadingDisable)
+  const [style, setStyle] = useState({})
+
+  useEffect(() => {
+    if (loading === true) {
+      setStyle({ opacity: '0' })
+    } else {
+      setStyle({ opacity: '1' })
+    }
+    return () => {
+    }
+  }, [loading])
 
   return (
-    <div className={classes.About}>
+    <div className={classes.About} style={style}>
       About page
     </div>
   )
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,4 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles({
+  About: {
+    width: '100%',
+    height: '100vh',
+    opacity: '0',
+    transition: 'opacity 0.8s ease-in-out'
+  }
 })
