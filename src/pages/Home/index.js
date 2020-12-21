725dbf09ff2d58ab3f339ec7b1446c78e5350927
DucSkii <ducvietdao@live.co.uk>
2020-12-21 01:56:03 +0000

    added animated gif onto home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, Button } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import duckAnimation from '../../video/duck-animation.gif'
 
 import { useStyles } from './styles'
 
@@ -9,6 +11,18 @@ const Home = () => {
 
   return (
     <div className={classes.Home}>
+      <img src={duckAnimation} alt='Animated gif'
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: '-1',
+        }}
+      />
       <Grid container>
         <Grid item xs={12} style={{ height: '20vh' }} />
         <Grid container item xs={12}>
@@ -41,6 +55,19 @@ const Home = () => {
             <Typography variant='h5' color='primary' style={{ fontFamily: 'Roboto', marginTop: '10px' }}>
               Front End Developer / React Developer
           </Typography>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} style={{ height: '5vh' }} />
+        <Grid container item xs={12}>
+          <Grid item xs={1} />
+          <Grid container item xs={11}>
+            <Link to='/contact' style={{ textDecoration: 'none' }}>
+              <Button variant='outlined' color='primary'>
+                <Typography variant='h6' style={{ fontFamily: 'Roboto', fontWeight: 'bold' }}>
+                  Contact me!
+              </Typography>
+              </Button>
+            </Link>
           </Grid>
         </Grid>
       </Grid>
