ddb86cba9b2f7f89a570596c0ac041348917a4e0
DucSkii <ducvietdao@live.co.uk>
2020-12-25 13:59:51 +0000

    form now responsive

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -1,21 +1,30 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-export const useStyles = makeStyles({
+export const useStyles = makeStyles(theme => ({
   input: {
     width: '100%',
     padding: '0px 6px',
-    height: '45px',
     fontFamily: 'Roboto',
     fontWeight: 'bold',
+    height: '30px',
+    fontSize: '12px',
+    [theme.breakpoints.up('md')]: {
+      height: '45px',
+      fontSize: '16px',
+    },
   },
   inputMessage: {
     width: '100%',
     padding: '10px',
-    height: '120px',
     borderColor: 'transparent',
     outline: 'none',
     fontFamily: 'Roboto',
-    fontSize: '16px',
     overflowY: 'auto',
+    fontSize: '12px',
+    height: '50px',
+    [theme.breakpoints.up('md')]: {
+      height: '120px',
+      fontSize: '16px',
+    },
   },
-})
+}))
